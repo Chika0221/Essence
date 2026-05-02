fac8c3b831e129d95b64bc90382c84636dd2fc48
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2026-05-02 13:15:02 +0900

    add: ollama APIのライフサイクル管理を追加し、モデル名を更新

--- Infrastructure/api/ollama_test.py
+++ Infrastructure/api/ollama_test.py
@@ -1,9 +1,29 @@
+import subprocess
 from fastapi import *
+# from fastapi.testclient import TestClient
+from contextlib import asynccontextmanager
 import ollama
 
-LLM_MODEL_NAME = "functiongemma:270m"
+LLM_MODEL_NAME = "granite4.1:3b"
+ollama_proc = None
 
-app = FastAPI(title="ollama API")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global ollama_proc
+    ollama_proc = subprocess.Popen(["ollama", "serve"])
+    try:
+        yield
+    finally:
+
+        print("API終了")
+        if ollama_proc and ollama_proc.poll() is None:
+            ollama_proc.terminate()
+            try:
+                ollama_proc.wait(timeout=5)
+            except subprocess.TimeoutExpired:
+                ollama_proc.kill()
+
+app = FastAPI(title="ollama API", lifespan=lifespan)
 
 
 @app.post("/api/v1/ollama")
